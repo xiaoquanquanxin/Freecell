26e37e87132b999c45e3883a448bbf4042c63693
xiaoquanquanxin <xxkl123@126.com>
2018-12-02 12:11:36 +0800

    错误：随机算法不对

--- core.js
+++ core.js
@@ -7,16 +7,19 @@ function CoreInit(tagName, type) {
 (function () {
 	CoreInit.prototype.setType = function (typeName, typeValue) {
 		this.element.dataset[typeName] = typeValue;
+		return this;
 	};
 	CoreInit.prototype.getType = function (typeName) {
 		return this.element.dataset.typeName;
 	};
+	//  DOM
 	CoreInit.prototype.appendTo = function (aim) {
 		if (aim instanceof Element && aim !== this.element) {
 			aim.appendChild(this.element);
 		} else {
 			throw new Error('\n错误记录：CoreInit.prototype.appendTo\n错误原因：错误的DOM目标对象或无法添加自身');
 		}
+		return this;
 	};
 	CoreInit.prototype.removeFrom = function (aim) {
 		if (aim instanceof Element && aim !== this.element) {
@@ -28,14 +31,16 @@ function CoreInit(tagName, type) {
 		} else {
 			throw new Error('\n错误记录：CoreInit.prototype.removeFrom\n错误原因：错误的DOM目标对象或无法移除自身');
 		}
-	}
-
+		return this;
+	};
+	//  CLASS
 	CoreInit.prototype.addClass = function (cssName) {
 		if (typeof cssName === 'string') {
 			this.element.classList.add(cssName)
 		} else {
 			throw new Error('\n错误记录：CoreInit.prototype.addClass\n错误原因：添加css类名必须为字符串');
 		}
+		return this;
 	};
 	CoreInit.prototype.removeClass = function (cssName) {
 		if (cssName === undefined) {
@@ -45,6 +50,7 @@ function CoreInit(tagName, type) {
 		} else {
 			throw new Error('\n错误记录：CoreInit.prototype.removeClass\n错误原因：删除css类名必须为字符串');
 		}
+		return this;
 	};
 	CoreInit.prototype.hasClass = function (cssName) {
 		if (typeof cssName === 'string') {
@@ -54,13 +60,32 @@ function CoreInit(tagName, type) {
 		} else {
 			throw new Error('\n错误记录：CoreInit.prototype.hasClass\n错误原因：查询css类名必须为字符串');
 		}
-	}
+	};
+	//  CSS
+	CoreInit.prototype.css = function (cssName, cssValue) {
+		if (typeof cssName === 'string') {
+			if (typeof cssValue === 'string') {
+				this.element.style[cssName] = cssValue;
+			} else if (cssValue === undefined) {
+				return getComputedStyle(this.element)[cssName];
+			} else {
+				throw new Error('\n错误记录：CoreInit.prototype.css\n错误原因：参数cssValue错误');
+			}
+		} else {
+			throw new Error('\n错误记录：CoreInit.prototype.css\n错误原因：无法识别的参数');
+		}
+		return this;
+	};
+	CoreInit.prototype.designArray = ['spade', 'heart', 'club', 'diamond'];
+	CoreInit.prototype.designIndex = 3;
+	CoreInit.prototype.pointsArray = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 }());
 
 
 //  牌 card
-function Card(param) {
+function Card(index) {
 	CoreInit.call(this, 'div', 'card');
+	this.drawPoint(index);
 }
 
 (function () {
@@ -68,15 +93,34 @@ function Card(param) {
 	};
 	Inh.prototype = CoreInit.prototype;
 	Card.prototype = new Inh();
+	Card.prototype.setTotalCard = function () {
+		var arr = [];
+		CoreInit.prototype.designArray.forEach(function (dt) {
+			CoreInit.prototype.pointsArray.forEach(function (pt) {
+				arr.push([pt, dt]);
+			})
+		});
+		arr = arr.map(function (t, i, arr) {
+			var spliceIndex = parseInt(Math.random() * 52);
+			var item = arr[spliceIndex];
+			arr[spliceIndex] = t;
+			return item;
+		});
+		arr.forEach(function (t) {
+			if( t[1] === "diamond"){
+				console.log(t);
+			}
+		})
+		return arr;
+	};
+	Card.prototype.getTotalCount = function () {
+		return Card.prototype.setTotalCard.length
+	};
+
+	Card.prototype.drawPoint = function (index) {
+		// console.log(index);
+	}
 }());
-
-// Card.prototype.init = function () {
-//     this.element.dataset.type = 'card';
-// };
-
-// var card = new Card();
-// console.log(card);
-
 
 //  位置
 function Seat(only, design) {
@@ -95,11 +139,10 @@ function Seat(only, design) {
 	};
 	Inh.prototype = CoreInit.prototype;
 	Seat.prototype = new Inh();
-	Seat.prototype.designArray = ['spade', 'heart', 'club', 'diamond'];
-	Seat.prototype.designIndex = 0;
 	Seat.prototype.drawDesign = function () {
-		var type = this.designArray[Seat.prototype.designIndex++];
+		var type = this.designArray[CoreInit.prototype.designIndex--];
 		this.setType('designType', type);
+		this.addClass(type);
 	};
 
 }());
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 window.onload = function () {
 	var app = App();
+	app.mobileResize();
+	window.onresize = function () {
+		app.mobileResize();
+	}
 	if (app.isMobile()) {
 		if (app.isLandscape()) {
 			// alert('请将屏幕横向翻转')
@@ -8,37 +12,48 @@ window.onload = function () {
 		}
 	} else {
 		// console.log('pc端');
+
+	}
+	//  创造位置
+	(function () {
 		//  空挡牌位
-
-	}
-	var spaceSeat = document.getElementById('spaceSeat');
-	for (var i = 0; i < 4; i++) {
-		var seat = new Seat('only');
-		// console.log(seat.element.dataset);
-		seat.addClass('spaceSeat');
-		seat.addClass('fl');
-		seat.appendTo(spaceSeat);
-	}
-	var placementSeat = document.getElementById('placementSeat');
-
-	for (var i = 0; i < 4; i++) {
-		var seat = new Seat('only', 'design');
-		// console.log(seat.element.dataset);
-		seat.addClass('placementSeat');
-		seat.addClass('fr');
-		seat.appendTo(placementSeat);
-	}
-	var operatingSeat = document.getElementById('operatingSeat');
-	for (var i = 0; i < 8; i++) {
-		var seat  = new Seat();
-		console.log(seat.element.dataset);
-		seat.addClass('fl');
-		seat.appendTo(operatingSeat);
-	}
-
-
-	app.mobileResize();
-	window.onresize = function () {
-		app.mobileResize();
-	}
+		var spaceSeat = document.getElementById('spaceSeat');
+		for (var i = 0; i < 4; i++) {
+			var seat = new Seat('only');
+			// console.log(seat.element.dataset);
+			seat.addClass('spaceSeat');
+			seat.addClass('fl');
+			seat.appendTo(spaceSeat);
+		}
+		//  安置区
+		var placementSeat = document.getElementById('placementSeat');
+		for (var i = 0; i < 4; i++) {
+			var seat = new Seat('only', 'design');
+			seat.addClass('placementSeat');
+			seat.addClass('fr');
+			seat.appendTo(placementSeat);
+		}
+		//  操作区
+		var operatingSeat = document.getElementById('operatingSeat');
+		for (var i = 0; i < 8; i++) {
+			var seat = new Seat();
+			seat.addClass('fl');
+			seat.appendTo(operatingSeat);
+		}
+	}());
+	//  创造牌
+	(function () {
+		var operatingSeat = document.getElementById('operatingSeat');
+		var seats = operatingSeat.getElementsByClassName('seat');
+		var total = Card.prototype.getTotalCount();
+		var index = 0;
+		while (index < total) {
+			var seatDiv = seats[index % 8];
+			var card = new Card(index);
+			card.addClass('card');
+			card.appendTo(seatDiv);
+			card.css('top', parseInt(index / 8) * 0.2 + 'rem');
+			index++;
+		}
+	}());
 };
